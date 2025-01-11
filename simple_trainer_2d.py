06fa5620e84e41f9f310f2e24d683719ee8d8bae
Nikolai Kochetkov <professor3222@gmail.com>
2025-01-11 19:55:32 +0000

    adding checkpoint saving and resuming training from checkpoint

--- simple_trainer_2d.py
+++ simple_trainer_2d.py
@@ -27,6 +27,8 @@ class Config:
     model_type: Literal["3dgs", "2dgs"] = "3dgs"
     save_steps: List[int] = field(default_factory=lambda: [7_000, 30_000])
     img_path: str = ""
+    ckpt_path: str = ""
+    results_dir: str = "results_2d"
 
 
 class SimpleTrainer:
@@ -36,17 +38,56 @@ class SimpleTrainer:
         self,
         gt_image: Tensor,
         num_points: int = 2000,
+        ckpt_path: str = "",
+        lr: float = 0.01,
+        results_dir: str = "",
     ):
         self.device = torch.device("cuda:0")
         self.gt_image = gt_image.to(device=self.device)
         self.num_points = num_points
+        self.ckpt_dir = f"{results_dir}/ckpts"
+        os.makedirs(self.ckpt_dir, exist_ok=True)
+        # self.stats_dir = f"{results_dir}/stats"
+        # os.makedirs(self.stats_dir, exist_ok=True)
+        # self.render_dir = f"{results_dir}/renders"
+        # os.makedirs(self.render_dir, exist_ok=True)
 
         fov_x = math.pi / 2.0
         self.H, self.W = gt_image.shape[0], gt_image.shape[1]
         self.focal = 0.5 * float(self.W) / math.tan(0.5 * fov_x)
         self.img_size = torch.tensor([self.W, self.H, 1], device=self.device)
 
-        self._init_gaussians()
+        if ckpt_path:
+            self._load_gaussians(ckpt_path)
+        else:
+            self._init_gaussians()
+
+        self.optimizer = optim.Adam(
+            [self.rgbs, self.means, self.scales, self.opacities, self.quats], lr
+        )
+
+        if ckpt_path:
+            self.optimizer.load_state_dict(self.optimizer_state_dict)
+
+    def _load_gaussians(self, ckpt_path):
+        ckpt = torch.load(ckpt_path, weights_only=True)
+        print(f"Loading checkpoint {ckpt_path}")
+        self.opacities = ckpt["opacities"]
+        self.quats = ckpt["quats"]
+        self.rgbs = ckpt["rgbs"]
+        self.scales = ckpt["scales"]
+        self.means = ckpt["means"]
+
+        self.optimizer_state_dict = ckpt["optimizer"]
+        self.viewmat = torch.tensor(
+            [
+                [1.0, 0.0, 0.0, 0.0],
+                [0.0, 1.0, 0.0, 0.0],
+                [0.0, 0.0, 1.0, 8.0],
+                [0.0, 0.0, 0.0, 1.0],
+            ],
+            device=self.device,
+        )
 
     def _init_gaussians(self):
         """Random gaussians"""
@@ -93,13 +134,9 @@ class SimpleTrainer:
     def train(
         self,
         iterations: int = 1000,
-        lr: float = 0.01,
         save_imgs: bool = False,
         model_type: Literal["3dgs", "2dgs"] = "3dgs",
     ):
-        optimizer = optim.Adam(
-            [self.rgbs, self.means, self.scales, self.opacities, self.quats], lr
-        )
         mse_loss = torch.nn.MSELoss()
         frames = []
         times = [0] * 2  # rasterization, backward
@@ -136,16 +173,29 @@ class SimpleTrainer:
             torch.cuda.synchronize()
             times[0] += time.time() - start
             loss = mse_loss(out_img, self.gt_image)
-            optimizer.zero_grad()
+            self.optimizer.zero_grad()
             start = time.time()
             loss.backward()
             torch.cuda.synchronize()
             times[1] += time.time() - start
-            optimizer.step()
+            self.optimizer.step()
             print(f"Iteration {iter + 1}/{iterations}, Loss: {loss.item()}")
 
             if save_imgs and iter % 50 == 0:
                 frames.append((out_img.detach().cpu().numpy() * 255).astype(np.uint8))
+            if iter in [i - 1 for i in cfg.save_steps] or iter == iterations - 1:
+                print(f"Saving checkpoint at: {iter}")
+                to_save = {
+                    "optimizer": self.optimizer.state_dict(),
+                    "iter": iter,
+                    "means": self.means,
+                    "quats": self.quats,
+                    "opacities": self.opacities,
+                    "rgbs": self.rgbs,
+                    "scales": self.scales,
+                }
+                torch.save(to_save, f"{self.ckpt_dir}/ckpt_{iter}.pt")
+
         if save_imgs:
             # save them as a gif with PIL
             frames = [Image.fromarray(frame) for frame in frames]
@@ -185,10 +235,15 @@ def main(
         gt_image[: cfg.height // 2, : cfg.width // 2, :] = torch.tensor([1.0, 0.0, 0.0])
         gt_image[cfg.height // 2 :, cfg.width // 2 :, :] = torch.tensor([0.0, 0.0, 1.0])
 
-    trainer = SimpleTrainer(gt_image=gt_image, num_points=cfg.num_points)
+    trainer = SimpleTrainer(
+        gt_image=gt_image,
+        num_points=cfg.num_points,
+        lr=cfg.lr,
+        ckpt_path=cfg.ckpt_path,
+        results_dir=cfg.results_dir,
+    )
     trainer.train(
         iterations=cfg.iterations,
-        lr=cfg.lr,
         save_imgs=cfg.save_imgs,
         model_type=cfg.model_type,
     )
