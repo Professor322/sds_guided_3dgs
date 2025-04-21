df5a9848fb9d1984b5df7a60aaf30b1dc6513ff7
Nikolai Kochetkov <professor3222@gmail.com>
2025-04-21 20:53:36 +0100

    validate more often to check if we are missing the right checkpoint

--- configs.py
+++ configs.py
@@ -44,7 +44,9 @@ class Config3D:
     # Number of training steps
     max_steps: int = 30_000
     # Steps to evaluate the model
-    eval_steps: List[int] = field(default_factory=lambda: [3_000, 7_000, 30_000])
+    eval_steps: List[int] = field(
+        default_factory=lambda: [3_000, 7_000, 10_000, 15_000, 30_000]
+    )
     # Steps to save the model
     save_steps: List[int] = field(default_factory=lambda: [3_000, 7_000, 30_000])
 
